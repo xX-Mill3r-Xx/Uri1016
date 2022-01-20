c7b6cae91eaf61ec4087872738235e0935d6be2f
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-01-20 17:04:59 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uri1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+            Console.Write("Entre com a distancia percorrida: ");
+            int distancia = int.Parse(Console.ReadLine());
+
+            y = distancia * 2;
+
+            Console.WriteLine($"O carro Y percorreu {y} minutos");
+            Console.ReadLine();
+        }
+    }
+}
